a6826d904a99697d21020eb265f3290164a21084
David Jaramillo <djdavidjaramillo@gmail.com>
2026-05-07 19:03:23 -0500

    Primera pantalla principal

--- app/src/main/java/com/david/leccion/MainActivity.kt
+++ app/src/main/java/com/david/leccion/MainActivity.kt
@@ -9,24 +9,35 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.david.leccion.ui.theme.LeccionTheme
 
-//Mis importaciones
-import android.R.attr.onClick
+// Mis imports
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import android.widget.ImageView
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,11 +45,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LeccionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = { NavbarArriba() }
+                ) { innerPadding ->
+                    Main(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -46,16 +57,15 @@ class MainActivity : ComponentActivity() {
 }
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Navbar(modifier: Modifier = Modifier) {
-    CenterAlignedTopAppBar(
+fun NavbarArriba(modifier: Modifier = Modifier) {
+    TopAppBar(
         title = {
             Row(
                 verticalAlignment = Alignment.CenterVertically
             ){
                 Text(
-//                    text = stringResource(R.string.app_name),
                     text = "Categoria",
-                    style = MaterialTheme.typography.displayLarge
+                    textAlign = TextAlign.Left,
                 )
             }
         },
@@ -63,47 +73,77 @@ fun Navbar(modifier: Modifier = Modifier) {
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Main(modifier: Modifier = Modifier) {
-    CenterAlignedTopAppBar(
-        title = {
-            Column(){
-                Text(
-//                    text = stringResource(R.string.app_name),
-                    text = "Bienvenido al catalogo General",
-                    style = MaterialTheme.typography.displayLarge
-                )
-                Text(
-                    text = "Explota y descubre detalles  sobre multiples categorias. Tu guia movil interactiva ",
-                    style = MaterialTheme.typography.displayLarge
-                )
-                Button() {
-
-                    Text(
-                        text = "Ver catalogo (grid)"
-                    )
-
-                }
-            }
-        },
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.libro_interfaz1),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = "Bienvenido al catalogo General",
+            style = MaterialTheme.typography.headlineLarge,
+            fontWeight = FontWeight.Bold,
+            fontSize = 45.sp,
+            textAlign = TextAlign.Center,
+            lineHeight = 40.sp
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = "Explota y descubre detalles sobre multiples categorias. Tu guia movil interactiva ",
+            style = MaterialTheme.typography.titleMedium,
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+            )
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Button(
+            onClick = {}
+        ) {
+
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                contentDescription = null
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Text(
+                text = "Ver catálogo (grid)",
+                fontSize = 25.sp,
+                modifier = Modifier.padding(10.dp)
+
+            )
+        }
+    }
 }
 
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    LeccionTheme {
-        Greeting("Android")
-    }
+//    LeccionTheme {
+//        Greeting("Android")
+//    }
 }
